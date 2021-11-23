3e16a53b393581a9d8dca2d25ac1bc439f5566c9
yeana0324 <yeana.dev@gmail.com>
2021-11-23 17:01:32 -0600

    Imported schemas

--- client/aajc/schemas/schema.js
+++ client/aajc/schemas/schema.js
@@ -6,9 +6,10 @@ import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // We import object and document schemas
 import blockContent from "./blockContent";
-import category from "./category";
-import post from "./post";
+import news from "./news";
 import affiliate from "./affiliate";
+import event from "./event";
+import donation from "./donation";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -19,9 +20,10 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    post,
     affiliate,
-    category,
+    news,
+    event,
+    donation,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
