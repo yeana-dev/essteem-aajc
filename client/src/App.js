772096b98187e7cadb38b6c3db3496ed055c46c3
jhuangliu <95jhliu@gmail.com>
2021-11-23 16:23:55 -0500

    added new folder and home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,42 @@
 import "./App.css";
-
+import HomeAffiliates from "./components/HomeAffiliates";
+const HomeAffiliates = [
+	{
+		name: 'yoyoyo',
+		image: 'yoyoy',
+		statement: 'yoyoyo',
+		link: 'yoyoyo',
+	},
+	{
+		name: 'yoyoyo',
+		image: 'yoyoy',
+		statement: 'yoyoyo',
+		link: 'yoyoyo',
+	},
+	{
+		name: 'yoyoyo',
+		image: 'yoyoy',
+		statement: 'yoyoyo',
+		link: 'yoyoyo',
+	},
+	{
+		name: 'yoyoyo',
+		image: 'yoyoy',
+		statement: 'yoyoyo',
+		link: 'yoyoyo',
+	},
+	{
+		name: 'yoyoyo',
+		image: 'yoyoy',
+		statement: 'yoyoyo',
+		link: 'yoyoyo',
+	},
+];
 function App() {
   return <div className="App"></div>;
+
+
+
 }
 
 export default App;
