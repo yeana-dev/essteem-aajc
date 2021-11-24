9280090e07dbea2488e9f580d63b3140eee8acb5
yeana0324 <yeana.dev@gmail.com>
2021-11-24 13:53:37 -0600

    Imported News Data from studio

--- client/src/screens/News/News.jsx
+++ client/src/screens/News/News.jsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react";
+import sanityClient from "../../Client";
+// import NewsCard from "../../components/NewsCard/NewsCard";
+
 export default function News(props) {
-  return <div>I am news</div>;
+  const [news, setNews] = useState(null);
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "news"]{
+      title,
+      slug,
+      affiliate,
+      publishedAt,
+      body,
+      mainImage{
+        asset->{
+          _id,
+          url
+        },
+      },
+      link
+    }`
+      )
+      .then((data) => setNews(data))
+      .catch(console.error);
+  }, []);
+  console.log(news);
+  return <div></div>;
 }
