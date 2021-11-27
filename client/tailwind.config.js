b5abacd088c88b336ebe3df2d5248b6dc127cab3
yeana0324 <yeana.dev@gmail.com>
2021-11-27 13:38:07 -0600

    Added customized color palatte

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,23 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: colors.white,
+      aajc: {
+        orange: "#F57B20",
+        darkorange: "#CE5800",
+        lightorange: "#F8B96F",
+        darkgrey: "#292929",
+        lightblue: "#BFE8EA",
+        yellowWhite: "#FDF4E6",
+      },
+    },
   },
   variants: {
     extend: {},
