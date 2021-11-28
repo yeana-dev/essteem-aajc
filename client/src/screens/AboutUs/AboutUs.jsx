1f8326bac2abb525c07514efd48c9d81f7f5ac1a
yeana0324 <yeana.dev@gmail.com>
2021-11-28 01:18:05 -0600

    Removed unused library

--- client/src/screens/AboutUs/AboutUs.jsx
+++ client/src/screens/AboutUs/AboutUs.jsx
@@ -1,15 +1,10 @@
 import { useState, useEffect } from "react";
 import sanityClient from "../../Client";
-import imageUrlBuilder from "@sanity/image-url";
-
 import "./AboutUs.css";
 
 export default function AboutUs(props) {
   const [affiliates, setAffiliates] = useState(null);
-  const builder = imageUrlBuilder(sanityClient);
-  function urlFor(source) {
-    return builder.image(source);
-  }
+
   useEffect(() => {
     sanityClient
       .fetch(
@@ -30,6 +25,7 @@ export default function AboutUs(props) {
       .then((data) => setAffiliates(data))
       .catch(console.error);
   }, []);
+
   return (
     <div>
       <div>
@@ -47,25 +43,7 @@ export default function AboutUs(props) {
           </p>
         </header>
         {affiliates &&
-          affiliates.map((affiliate) => (
-            <div className="bg-aajc-lightorange">
-              <article className="flex max-w-6xl mx-auto px-4 gap-10 items-center">
-                <img
-                  src={urlFor(affiliate.image.asset.url).url()}
-                  alt=""
-                  className="city-image"
-                />
-                <section>
-                  <h1>{affiliate.name}</h1>
-                  <p>{affiliate.bio}</p>
-                  <p>
-                    To learn more about our work and donate, please visit our{" "}
-                    <a href={affiliate.link}>main site</a>.
-                  </p>
-                </section>
-              </article>
-            </div>
-          ))}
+          affiliates.map((affiliate) => <div>{affiliate.name}</div>)}
       </div>
     </div>
   );
