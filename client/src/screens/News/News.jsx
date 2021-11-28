3c17f59c9e14e9fa7aa5fd157077308d8b061173
yeana0324 <yeana.dev@gmail.com>
2021-11-27 20:48:24 -0600

    Created select option for locations on search form

--- client/src/screens/News/News.jsx
+++ client/src/screens/News/News.jsx
@@ -3,6 +3,7 @@ import sanityClient from "../../Client";
 import NewsCard from "../../components/NewsCard/NewsCard";
 
 export default function News(props) {
+  let locations = [];
   const [news, setNews] = useState(null);
   useEffect(() => {
     sanityClient
@@ -23,14 +24,38 @@ export default function News(props) {
       .then((data) => setNews(data))
       .catch(console.error);
   }, []);
-  console.log(news);
+
+  if (news && news.length > 0) {
+    news.map((each) => {
+      if (!locations.includes(each.affiliate.location)) {
+        return locations.push(each.affiliate.location);
+      }
+    });
+  }
+
   return (
     <div className="max-w-6xl mx-auto px-4 py-10">
       <header>
-        <h1 className="font-bold text-2xl">News & Press Releases</h1>
+        <h1 className="font-bold text-3xl">News & Press Releases</h1>
       </header>
+      <form>
+        <label for="search" />
+        <input
+          type="text"
+          name="search"
+          placeholder="Search for News & Press Releases"
+        />
+        <select>
+          <option value="" selected disabled hidden>
+            Location
+          </option>
+          {locations.map((location) => (
+            <option>{location}</option>
+          ))}
+        </select>
+      </form>
       <main>
-        <h2 className="uppercase text-xl">Latest</h2>
+        <h2 className="uppercase text-lg font-semibold py-5">Latest</h2>
         <div className="flex justify-between flex-wrap gap-10">
           {news && news.map((each) => <NewsCard key={each._id} news={each} />)}
         </div>
