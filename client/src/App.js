b7bf7be88c1ed5ad56382bc72ac6d35d28ed66bc
yeana0324 <yeana.dev@gmail.com>
2021-11-28 01:52:48 -0600

    Added additional routes

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import HomeIcons from "./components/HomeIcons";
 import Layout from "./layout/Layout";
 import Home from "./screens/Home/Home";
 import AboutUs from "./screens/AboutUs/AboutUs";
+import SingleAboutUs from "./screens/SingleAboutUs/SingleAboutUs";
 import News from "./screens/News/News";
 import Events from "./screens/Events/Events";
 import Donate from "./screens/Donate/Donate";
@@ -74,7 +75,12 @@ function App() {
         <HomeIcons homeIcons={homeIcons} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/aout-us" element={<AboutUs />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/about-us/atlanta" element={<SingleAboutUs />} />
+          <Route path="/about-us/chicago" element={<SingleAboutUs />} />
+          <Route path="/about-us/los-angeles" element={<SingleAboutUs />} />
+          <Route path="/about-us/san-francisco" element={<SingleAboutUs />} />
+          <Route path="/about-us/washington-dc" element={<SingleAboutUs />} />
           <Route path="/news" element={<News />} />
           <Route path="/events" element={<Events />} />
           <Route path="/donate" element={<Donate />} />
