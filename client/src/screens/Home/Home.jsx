50f3339fd4a84a4051ee2782123b58c2da23eeb6
yeana0324 <yeana.dev@gmail.com>
2021-11-28 17:51:35 -0600

    Added carousel

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,3 +1,26 @@
+import HomeSlider from "../../components/HomeSlider/HomeSlider";
+import { useState, useEffect } from "react";
+import sanityClient from "../../Client";
+
 export default function Home() {
-  return <div>I am home</div>;
+  const [slides, setSlides] = useState(null);
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "carousel"]{
+      heading,
+      subHeading,
+      link,
+      image{
+        asset->{
+          _id,
+          url
+        },
+      },
+    }`
+      )
+      .then((data) => setSlides(data))
+      .catch(console.error);
+  }, []);
+  return <div>{slides && <HomeSlider slides={slides} />}</div>;
 }
