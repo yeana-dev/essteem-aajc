3b03ba1a9920b50173a66ae0ec65237b9cb0651e
yeana0324 <yeana.dev@gmail.com>
2021-11-28 16:23:01 -0600

    Installed bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 
 import ImageSilder from "./components/Carousel/ImageSilder";
