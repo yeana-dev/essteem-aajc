71318a2cebf498cd183ef54866f3430fa4ffe4a9
yeana0324 <yeana.dev@gmail.com>
2021-11-28 01:48:44 -0600

    Added redirecting each affiliates' link and styling

--- client/src/screens/AboutUs/AboutUs.jsx
+++ client/src/screens/AboutUs/AboutUs.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import sanityClient from "../../Client";
 import "./AboutUs.css";
 
@@ -28,9 +29,11 @@ export default function AboutUs(props) {
 
   return (
     <div>
-      <div>
-        <header className="max-w-6xl mx-auto px-4">
-          <h2>Together, we fight anti-Asian hate and discrimination.</h2>
+      <div className="max-w-6xl mx-auto px-4">
+        <header className="flex flex-col gap-7">
+          <h2 className="text-aajc-orange font-bold text-3xl text-center">
+            Together, we fight anti-Asian hate and discrimination.
+          </h2>
           <p>
             Asian Americans Advancing Justice (Advancing Justice) is a national
             affiliation of five civil rights nonprofit organizations.
@@ -42,8 +45,23 @@ export default function AboutUs(props) {
             Americans and Pacific Islanders and other underserved communities.
           </p>
         </header>
-        {affiliates &&
-          affiliates.map((affiliate) => <div>{affiliate.name}</div>)}
+        <div className="flex py-10">
+          {affiliates &&
+            affiliates.map((affiliate) => (
+              <div className="flex flex-col gap-2 items-center px-3">
+                <img src={affiliate.image.asset.url} />
+                <p className="text-sm font-semibold text-center">
+                  {affiliate.name}
+                </p>
+                <Link
+                  to={`/about-us/${affiliate.slug.current}`}
+                  className="text-xs underline uppercase"
+                >
+                  Learn More
+                </Link>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
