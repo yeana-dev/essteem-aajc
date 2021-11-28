3525fc4cd77b000855f8d5b33c71b0c1e6628c30
yeana0324 <yeana.dev@gmail.com>
2021-11-28 17:52:04 -0600

    Created carousel with imported data from sanity schema

---
+++ client/src/components/HomeSlider/HomeSlider.jsx
@@ -0,0 +1,21 @@
+import Carousel from "react-bootstrap/Carousel";
+import "./HomeSlider.css";
+
+export default function HomeSlider({ slides }) {
+  console.log(slides);
+  return (
+    <Carousel className="w-full mx-auto">
+      {slides.map((slide) => (
+        <Carousel.Item>
+          <a href={slide.link}>
+            <img className="d-block" src={slide.image.asset.url} alt="" />
+          </a>
+          <Carousel.Caption>
+            <h1 className="text-3xl font-bold mb-2">{slide.heading}</h1>
+            <h2>{slide.subHeading}</h2>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
