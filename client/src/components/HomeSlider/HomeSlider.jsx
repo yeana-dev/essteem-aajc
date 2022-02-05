5920922620b5a01948d035e322128c44a66dec0c
yeana0324 <yeana.dev@gmail.com>
2022-02-04 22:27:51 -0600

    Modified to enable onClick only if the slide has a redirect link

--- client/src/components/HomeSlider/HomeSlider.jsx
+++ client/src/components/HomeSlider/HomeSlider.jsx
@@ -1,5 +1,6 @@
 import Carousel from "react-bootstrap/Carousel";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import sanityClient from "../../client";
 import "./HomeSlider.css";
 
@@ -24,24 +25,34 @@ export default function HomeSlider() {
       .catch(console.error);
   }, []);
 
+  function carousel(slide) {
+    return (
+      <>
+        <img
+          className="d-block w-full h-96"
+          src={slide.image.asset.url}
+          alt=""
+        />
+        <Carousel.Caption className="flex flex-col justify-center gap-2 md:gap-5">
+          <h1 className="md:text-3xl text-xl font-bold md:mb-2 mb-10">
+            {slide.heading}
+          </h1>
+          <p>{slide.subHeading}</p>
+        </Carousel.Caption>
+      </>
+    );
+  }
+
   if (slides && slides.length > 0) {
     return (
       <Carousel className="carousel w-full h-max mx-auto object-none">
         {slides.map((slide) => (
           <Carousel.Item>
-            <a href={slide.link} target="_blank" rel="noreferrer">
-              <img
-                className="d-block w-full h-96"
-                src={slide.image.asset.url}
-                alt=""
-              />
-            </a>
-            <Carousel.Caption className="flex flex-col justify-center gap-2 md:gap-5">
-              <h1 className="md:text-3xl text-xl font-bold md:mb-2 mb-10">
-                {slide.heading}
-              </h1>
-              <h2>{slide.subHeading}</h2>
-            </Carousel.Caption>
+            {slide.link ? (
+              <Link to={slide.link}>{carousel(slide)}</Link>
+            ) : (
+              carousel(slide)
+            )}
           </Carousel.Item>
         ))}
       </Carousel>
