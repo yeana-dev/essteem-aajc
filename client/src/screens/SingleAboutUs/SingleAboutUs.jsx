3aafb79dc4848c60ed20d28d969ad1c3e0fda2cb
yeana0324 <yeana.dev@gmail.com>
2022-02-08 23:48:37 -0600

    Modified styling and responsive design

--- client/src/screens/SingleAboutUs/SingleAboutUs.jsx
+++ client/src/screens/SingleAboutUs/SingleAboutUs.jsx
@@ -32,11 +32,13 @@ export default function SingleAboutUs() {
 
   return (
     <div className="bg-aajc-lightorange">
-      <div className="max-w-6xl mx-auto lg:px-4">
+      <div className="max-w-6xl mx-auto">
         {affiliate && (
-          <div className="flex justify-between gap-10 items-center flex-col-reverse sm:flex-row">
-            <section className="flex flex-col justify-center items-center gap-10 py-5">
-              <h1 className="text-2xl font-bold">{affiliate.name}</h1>
+          <div className="flex justify-between gap-10 items-center flex-col-reverse md:flex-row">
+            <section className="flex flex-col justify-center items-center gap-10 p-5 md:px-0">
+              <h1 className="text-2xl font-bold tracking-tighter">
+                {affiliate.name}
+              </h1>
               <p className="leading-loose">{affiliate.bio}</p>
               <p className="text-sm font-semibold">
                 To learn more about our work and donate, please visit our{" "}
@@ -54,7 +56,7 @@ export default function SingleAboutUs() {
             <img
               src={affiliate.image.asset.url}
               alt=""
-              className="object-cover sm:w-1/2"
+              className="object-cover md:w-1/2"
             />
           </div>
         )}
