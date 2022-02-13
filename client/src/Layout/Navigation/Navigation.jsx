5dc9787b85bc00fb483194351d2d96d0988a52a1
yeana0324 <yeana.dev@gmail.com>
2022-02-12 18:33:14 -0600

    Modified html semantic tags

--- client/src/Layout/Navigation/Navigation.jsx
+++ client/src/Layout/Navigation/Navigation.jsx
@@ -15,16 +15,16 @@ export default function Navigation() {
     <nav className="bg-white uppercase" id="top">
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex space-x-7 justify-between">
-          <div>
+          <header>
             <NavLink to="/" className="flex items-center py-4 px-2">
               <img src={logo} alt="" width="250" />
             </NavLink>
-          </div>
+          </header>
 
-          <div className="hidden md:flex items-center space-x-4 wide-menu text-sm">
+          <section className="hidden md:flex items-center space-x-4 wide-menu text-sm">
             <NavLink
               to="/"
-              className="py-4 px-2 text-aajc-darkgrey font-semibold hover:text-black transition duration-300"
+              className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300"
             >
               Home
             </NavLink>
@@ -32,7 +32,7 @@ export default function Navigation() {
             <div className="relative group">
               <NavLink
                 to="about-us"
-                className="py-4 px-2 text-aajc-darkgrey font-semibold hover:text-aajc-orange transition duration-300"
+                className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300"
               >
                 About Us
               </NavLink>
@@ -41,31 +41,31 @@ export default function Navigation() {
                   <div className="grid grid-cols-1 ">
                     <NavLink
                       to="/about-us/atlanta"
-                      className="py-4 px-2 text-aajc-darkgrey hover:text-aajc-orange font-semibold transition duration-300"
+                      className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300"
                     >
                       Atlanta
                     </NavLink>
                     <NavLink
                       to="/about-us/chicago"
-                      className="py-4 px-2 text-aajc-darkgrey font-semibold hover:text-aajc-orange transition duration-300"
+                      className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300"
                     >
                       Chicago
                     </NavLink>
                     <NavLink
                       to="/about-us/los-angeles"
-                      className="py-4 px-2 text-aajc-darkgrey font-semibold hover:text-aajc-orange transition duration-300"
+                      className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300"
                     >
                       Los Angeles
                     </NavLink>
                     <NavLink
                       to="/about-us/san-francisco"
-                      className="py-4 px-2 text-aajc-darkgrey font-semibold hover:text-aajc-orange transition duration-300"
+                      className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300"
                     >
                       San Francisco
                     </NavLink>
                     <NavLink
                       to="/about-us/washington-dc"
-                      className="py-4 px-2 text-aajc-darkgrey font-semibold hover:text-aajc-orange transition duration-300"
+                      className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300"
                     >
                       Washington D.C.
                     </NavLink>
@@ -76,23 +76,23 @@ export default function Navigation() {
 
             <NavLink
               to="/news"
-              className="py-4 px-2 text-aajc-darkgrey font-semibold hover:text-aajc-orange transition duration-300"
+              className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300 "
             >
               News
             </NavLink>
             <NavLink
               to="/events"
-              className="py-4 px-2 text-aajc-darkgrey font-semibold hover:text-aajc-orange transition duration-300"
+              className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300"
             >
               Events
             </NavLink>
             <NavLink
               to="/donate"
-              className="py-4 px-2 text-aajc-darkgrey font-semibold hover:text-aajc-orange transition duration-300"
+              className="py-4 px-2 text-aajc-darkgrey font-semibold transition duration-300"
             >
               Donations
             </NavLink>
-          </div>
+          </section>
 
           <div className="md:hidden flex items-center">
             <button
@@ -120,15 +120,15 @@ export default function Navigation() {
       <section
         className={`${
           !toggle && "hidden"
-        } mobile-menu md:hidden px-3 border-t-8 border-aajc-orange`}
+        } md:hidden px-3 border-t-8 border-aajc-orange`}
         id="menu"
         onClick={() => hide()}
       >
-        <ul>
+        <ul className="mobile-menu">
           <li>
             <NavLink
               to="/"
-              className="block text-sm px-2 py-3 text-black hover:bg-black hover:text-white transition duration-300"
+              className="block text-sm px-2 py-3 transition duration-300"
             >
               Home
             </NavLink>
@@ -141,48 +141,47 @@ export default function Navigation() {
               About Us
             </NavLink>
           </li>
-          <ul>
-            <li>
-              <NavLink
-                to="/about-us/atlanta"
-                className="block text-sm px-2 py-3 transition duration-300"
-              >
-                Atlanta
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about-us/chicago"
-                className="block text-sm px-2 py-3 transition duration-300"
-              >
-                Chicago
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about-us/los-angeles"
-                className="block text-sm px-2 py-3 transition duration-300"
-              >
-                Los Angeles
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about-us/san-francisco"
-                className="block text-sm px-2 py-3 transition duration-300"
-              >
-                San Francisco
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about-us/washington-dc"
-                className="block text-sm px-2 py-3 transition duration-300"
-              >
-                Washington D.C.
-              </NavLink>
-            </li>
-          </ul>
+          <li>
+            <NavLink
+              to="/about-us/atlanta"
+              className="block text-sm px-2 py-3 transition duration-300"
+            >
+              Atlanta
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about-us/chicago"
+              className="block text-sm px-2 py-3 transition duration-300"
+            >
+              Chicago
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about-us/los-angeles"
+              className="block text-sm px-2 py-3 transition duration-300"
+            >
+              Los Angeles
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about-us/san-francisco"
+              className="block text-sm px-2 py-3 transition duration-300"
+            >
+              San Francisco
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about-us/washington-dc"
+              className="block text-sm px-2 py-3 transition duration-300"
+            >
+              Washington D.C.
+            </NavLink>
+          </li>
+
           <li>
             <NavLink
               to="/news"
