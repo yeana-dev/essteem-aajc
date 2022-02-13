74a6b9aea65a6713a52316a8cb6acdd1c2c4aa6f
yeana0324 <yeana.dev@gmail.com>
2022-02-12 18:33:35 -0600

    Modified html semantic tags

--- client/src/components/NewsCard/NewsCard.jsx
+++ client/src/components/NewsCard/NewsCard.jsx
@@ -1,27 +1,36 @@
-import "./NewsCard.css";
+import { Link } from "react-router-dom";
 
 export default function NewsCard({ news }) {
-  return (
-    <div className="news-card">
-      <a href={news.link} target="_blank" rel="noreferrer">
+  const card = (news) => {
+    return (
+      <div className="news-card">
         <header className="bg-aajc-yellowWhite rounded-lg p-3">
-          <div>
+          <ul className="flex flex-wrap gap-2">
             {news.affiliate.map((eachAffiliate) => (
-              <button className="uppercase bg-aajc-darkgrey text-white text-xs font-bold px-2 py-1 rounded-lg mr-2">
+              <li className="uppercase bg-aajc-darkgrey text-white text-xs font-bold px-2 py-1 rounded-lg">
                 {eachAffiliate.location}
-              </button>
+              </li>
             ))}
-            <button className="uppercase bg-aajc-lightorange text-aajc-darkgrey text-xs font-bold px-2 py-1 rounded-lg">
+            <li className="uppercase bg-aajc-lightorange text-aajc-darkgrey text-xs font-bold px-2 py-1 rounded-lg">
               {news.press_release ? "Press Release" : "News"}
-            </button>
-          </div>
+            </li>
+          </ul>
           <h1 className="pt-2 text-lg font-bold">{news.title}</h1>
         </header>
         <article>
           <p className="text-xs py-2 font-bold">{news.publishedAt}</p>
           <p className="text-sm">{news.body}</p>
         </article>
+      </div>
+    );
+  };
+  if (news.link) {
+    return (
+      <a href={news.link} target="_blank" rel="noreferrer">
+        {card(news)}
       </a>
-    </div>
-  );
+    );
+  } else {
+    return <Link to={`/news/${news.slug.current}`}>{card(news)}</Link>;
+  }
 }
