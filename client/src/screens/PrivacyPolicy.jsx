e422c201478073c725e7f0deba5e5ec1842d5876
Yeana Cho <YeanaCho@RP-YeanaCho.attlocal.net>
2022-02-13 23:36:28 -0600

    Added specific styling on the headers and a tags

--- client/src/screens/PrivacyPolicy.jsx
+++ client/src/screens/PrivacyPolicy.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import sanityClient from "../client";
 import PortableText from "react-portable-text";
+import "./styles/PrivacyPolicy.css";
 
 export default function PrivacyPolicy() {
   const [policy, setPolicy] = useState(null);
@@ -21,7 +22,10 @@ export default function PrivacyPolicy() {
   if (policy !== null) {
     return (
       <main className="max-w-6xl mx-auto p-10">
-        <PortableText content={policy.privacy_policy} />
+        <PortableText
+          className="privacy_policy"
+          content={policy.privacy_policy}
+        />
       </main>
     );
   } else {
