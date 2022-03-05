b9fc6f212fc415dbb22a2cc0ea7e8e3abab3b777
yeana-dev <yeana.dev@gmail.com>
2022-03-05 16:36:48 -0600

    Added descriptions and validations

--- client/advancingjustice/schemas/news.js
+++ client/advancingjustice/schemas/news.js
@@ -5,33 +5,50 @@ export default {
   fields: [
     {
       name: "title",
-      title: "Title",
+      title: "Title (Required)",
       type: "string",
+      validation: (Rule) => Rule.required().error("This field is required"),
     },
     {
       name: "slug",
-      title: "Slug",
+      title: "Slug (Required)",
+      description:
+        "https://advancingjustice.org/news/<This is where this slug goes to for this article>; 'Generate' button will autofill this. Must be unique.",
       type: "slug",
       options: {
         source: "title",
         maxLength: 96,
       },
+      validation: (Rule) => Rule.required().error("This field is required"),
     },
     {
       name: "affiliate",
-      title: "Affiliate",
+      title: "Affiliate (Required)",
+      description: "May be one or more",
       type: "array",
       of: [{ type: "reference", to: { type: "affiliate" } }],
+      validation: (Rule) => Rule.required().error("This field is required"),
     },
     {
       name: "press_release",
       title: "Press Release",
+      description: "Toggle if this article is a press release",
       type: "boolean",
     },
     {
       name: "publishedDate",
-      title: "Published Date",
+      title: "Published Date (Required)",
       type: "date",
+      validation: (Rule) => Rule.required().error("This field is required"),
+    },
+    {
+      name: "body",
+      title: "Short Description (Required)",
+      type: "text",
+      description:
+        "This is a preview of this article displayed on the news list.",
+      validation: (Rule) =>
+        Rule.required().min(1).max(200).error("Maximum 200 characters"),
     },
     {
       name: "link",
@@ -39,15 +56,6 @@ export default {
       description:
         "If this area is filled, User will be redirected to this site by clicking this news. Otherwise, please provide the article to the below 'Article' section.",
       type: "url",
-    },
-    {
-      name: "body",
-      title: "Body",
-      type: "text",
-      description:
-        "This is a preview of this article displayed on the news list.",
-      validation: (Rule) =>
-        Rule.required().max(200).error("Maximum 200 characters"),
     },
     {
       name: "article",
